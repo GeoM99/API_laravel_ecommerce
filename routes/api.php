e600c388e4b1a773a991e751a382f822c5f4cf39
GeoM99 <geo6641@gmail.com>
2018-10-27 17:01:19 -0600

    add controllers and routes resources

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Http\Request;
 
+Route::apiResource('/products', 'ProductController');
 
+Route::group(['prefix' => 'products'], function(){
+    Route::apiResource('/{product}/reviews', 'ReviewController');
+});
